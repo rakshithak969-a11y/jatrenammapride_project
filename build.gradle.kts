146c826ca748ccb3eedce1abdb9ff0495aa14b55
jatre namma pride <rakshithak969@gmail.com>
2026-05-19 16:48:52 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,100 @@
 plugins {
 
-    id("com.android.application") version "8.6.0" apply false
+    id("com.android.application")
 
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android")
 
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose")
 
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services")
+}
+
+android {
+
+    namespace = "com.example.jatrenammapride"
+
+    compileSdk = 34
+
+    defaultConfig {
+
+        applicationId = "com.example.jatrenammapride"
+
+        minSdk = 24
+
+        targetSdk = 34
+
+        versionCode = 1
+
+        versionName = "1.0"
+    }
+
+    buildTypes {
+
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+
+        sourceCompatibility = JavaVersion.VERSION_17
+
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+
+        compose = true
+    }
+
+    composeOptions {
+
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.13.1")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+
+    implementation("androidx.activity:activity-compose:1.9.1")
+
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+
+    implementation("androidx.compose.ui:ui")
+
+    implementation("androidx.compose.ui:ui-graphics")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.material:material-icons-extended")
+
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+
+    implementation("com.google.maps.android:maps-compose:4.4.1")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
